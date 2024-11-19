cb3f721e5d941692805b660a8e4143e5fb90f883
bokekez <bokekez@gmail.com>
2024-11-19 21:27:16 +0100

    routing fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/shop/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html', 
+      },
+    },
+  },
+  // server: {
+  //   historyApiFallback: true,
+  // },
 });
